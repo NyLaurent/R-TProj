697ee5e7449483c3dd29d95bff0a16a7f24a74ab
lauLanez <laulanyumbayire@gmail.com>
2024-05-17 09:32:16 -0500

    added about component

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Features from './components/Features'
+import About from './components/About'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <Home/>
      <Navbar/>
      <Features/>
+     <About/>
      
     </>
   )
---
+++ project/src/components/About.jsx
@@ -0,0 +1,16 @@
+import sitImg from '../assets/sit.png'
+
+const About = () => {
+    return (
+        <div className=''>
+            <div>
+                <div className='md:w-1/2'>
+                    <img src={sitImg} alt="" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
